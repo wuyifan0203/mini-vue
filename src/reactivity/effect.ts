9e685138689ad7508a79f29bd50e557b90f02504
wuyifan <1208097313@qq.com>
2023-01-11 00:50:27 +0800

    feat：完成 stop 和 onStop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,19 +1,40 @@
-class ReactiveEffect{
-    private _fn:any
+import { extend } from "../shared";
 
-    constructor(_fn){
-        this._fn = _fn
+class ReactiveEffect {
+    // 存放放dep
+    deps = [];
+    // 标志位是否执行过stop
+    active = true;
+    // 内部的fn
+    private _fn: any
+    // 是否传入过onStop ，在调用stop时执行
+    onStop?: () => void
+
+    constructor(_fn, public scheduler?) {
+        this._fn = _fn;
     }
 
-    run(){
+    run() {
         activeEffect = this
-         return this._fn();
+        return this._fn();
+    }
+
+    stop() {
+        if (this.active) {
+            this.active = false
+            this.deps.forEach((dep: any) => {
+                dep.delete(this)
+            });
+            if (this.onStop) {
+                this.onStop();
+            }
+        }
     }
 }
 
 const targetMap = new Map();
 
-export function track(target:any,key:any) {
+export function track(target: any, key: any) {
     // target 对应原始对象, key 对应当前修改的字段
     // 一个target 对应一个 key ，一个key对应一个dep；
     /**
@@ -25,35 +46,56 @@ export function track(target:any,key:any) {
      * 
      **/
     let depMap = targetMap.get(target);
-    if(!depMap){
+    if (!depMap) {
         depMap = new Map();
-        targetMap.set(target,depMap)
+        targetMap.set(target, depMap)
     }
 
     let dep = depMap.get(key);
-    if(!dep){
+    if (!dep) {
         dep = new Set();
-        depMap.set(key,dep)
+        depMap.set(key, dep)
     }
+
+    // 当收集时，没有effect的时候，需要返回
+    if(!activeEffect) return
     // 现在执行的effect 为当前的activeEffect(当前依赖项)
-    dep.add(activeEffect)
+    dep.add(activeEffect);
+    // 反向收集dep，使effect 有dep
+    activeEffect.deps.push(dep);
 }
 
-export function trigger(target,key) {
+export function trigger(target, key) {
     let depMap = targetMap.get(target);
     let dep = depMap.get(key);
 
     for (const effect of dep) {
-        effect.run()
+        if (effect.scheduler) {
+            effect.scheduler();
+
+        } else {
+            effect.run()
+        }
+
     }
-    
+
 }
 let activeEffect;
-export function effect(fn) {
-    const _effect = new ReactiveEffect(fn);
+export function effect(fn, options: any = {}) {
+    const _effect = new ReactiveEffect(fn, options.scheduler);
+    extend(_effect, options);
 
     _effect.run()
 
-    return _effect.run.bind(_effect)
-    
+    const runner: any = _effect.run.bind(_effect);
+    // 反向收集，使runner拥有effect属性
+    runner.effect = _effect;
+
+    return runner;
+
+}
+
+export function stop(runner) {
+    // 实现 stop 只需要删除对应dep里的efffect函数
+    runner.effect.stop();
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,25 +1,25 @@
-import { effect } from "../effect";
+import { effect,stop } from "../effect";
 import { reactive } from "../reactive";
 describe('effect', () => {
     it('happy path', () => {
 
         // 执行effect函数，会调用传的fn
         // reactive对象 . 会调用get方法
-        // 赋值时会调用set 方法
+        // 赋值时会调用set 方法,set会执行effect的fn
         const user = reactive({
-            age:10
+            age: 10
         });
 
         let nextAge;
-        const fn = ()=>{
-            nextAge = user.age +1
+        const fn = () => {
+            nextAge = user.age + 1
         }
+        // 执行effect 会调用fn
         effect(fn)
-
         expect(nextAge).toBe(11)
         // update
 
-        user.age ++;
+        user.age++;
         expect(nextAge).toBe(12);
         expect(user.age).toBe(11)
     });
@@ -29,7 +29,7 @@ describe('effect', () => {
         // 这个返回的函数叫runner
         //执行runner会调用effect的fn函数，返回fn执行后的值
         let foo = 1;
-        const runner = effect(()=>{
+        const runner = effect(() => {
             foo++;
             return 'foo'
         })
@@ -38,6 +38,78 @@ describe('effect', () => {
         const res = runner();
         expect(foo).toBe(3);
         expect(res).toBe('foo');
+
+    });
+
+    it('scheduler', () => {
+        // 1. 通过 effect 的第二个参数，给 scheduler 传一个 fn作为参数
+        // 2. effect 第一次执行的时候 还会执行fn
+        // 3.当响应式对象 触发set 并更新时不会执行fn，而是执行 scheduler
+        // 4.当执行runner 会再次执行fn
+
+        let dummy;
+        let run;
+        const scheduler = jest.fn(() => {
+            run = runner;
+        });
+        const obj = reactive({ foo: 1 });
+
+        const runner = effect(() => {
+            dummy = obj.foo;
+        }, { scheduler });
+
+        expect(scheduler).not.toHaveBeenCalled();
+        expect(dummy).toBe(1);
+        obj.foo++;
+        expect(scheduler).toHaveBeenCalledTimes(1);
+        expect(dummy).toBe(1);
+        run()
+        expect(dummy).toBe(2);
+    });
+
+    it('stop', () => {
+        let dummy;
+        const obj = reactive({ foo: 1 });
+        const runner = effect(() => {
+            dummy = obj.foo;
+        });
+
+        obj.foo =2;
+        // 赋值操作，触发effect 的fn
+        expect(dummy).toBe(2);
+        stop(runner);
+        obj.foo=3;
+        // stop 后，不会调用 efffect 的fn
+        expect(dummy).toBe(2);
+
+
+        runner();
+        // 执行runner 会调用effect的fn
+        expect(dummy).toBe(3);
+
+    });
+
+    it('should call onStop when use stop', () => {
+        const user = reactive({
+            age:23
+        });
+
+        const onStop = jest.fn();
+
+        let newAge;
+
+        const runner = effect(()=>{
+            newAge = user.age;
+        },
+        {
+            onStop
+        })
+        
+        user.age = 24;
+        expect(newAge).toBe(24);
+
+        stop(runner);
+        expect(onStop).toBeCalledTimes(1);
 
     });
 });
